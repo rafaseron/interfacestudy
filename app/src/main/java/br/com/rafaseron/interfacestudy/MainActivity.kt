160eb41e08b36b954da6459240c07809df905fc2
rafaseron <rafaelseron3011@gmail.com>
2023-10-27 08:32:14 -0400

    passando valor da View para kt - aula 9

--- app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
@@ -2,10 +2,33 @@ package br.com.rafaseron.interfacestudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    //inicializamos as variveis e typamos
+    lateinit var edtKHWPrice: EditText
+    lateinit var btnCalculate: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //fizemos as variaveis (já typadas) receberem valores (no caso, Views)
+        //dessa maneira ligamos a View com o codigo em Kotlin (com as variaveis)
+        fun setupViews (){
+        edtKHWPrice = findViewById(R.id.edtKHWPrice)
+        btnCalculate = findViewById(R.id.btnCalculate)
+        }
+        setupViews()
+
+        //agora vamos começar a usar Listeners para monitorar eventos de clicks
+        fun setupListeners(){
+        btnCalculate.setOnClickListener{
+            Log.d("BOTAO CALCULATE","texto capturado -> ${edtKHWPrice.text.toString()}")
+        }
+        }
+        setupListeners()
     }
 }
