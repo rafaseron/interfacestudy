809597b48b20a313f739b3799647eb77eb553cf1
rafaseron <rafaelseron3011@gmail.com>
2023-10-29 15:58:30 -0400

    outros tipos de view

--- app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Switch
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     //inicializamos as variveis e typamos
     lateinit var edtKHWPrice: EditText
     lateinit var btnCalculate: Button
+    lateinit var radioGroup: RadioGroup
+    lateinit var checkBox1: CheckBox
+    lateinit var checkBox2: CheckBox
+    lateinit var switch: Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
