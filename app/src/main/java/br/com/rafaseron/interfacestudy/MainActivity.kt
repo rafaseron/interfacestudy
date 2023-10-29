d0b9374cfe34978172be748a0a16ff048ac793d8
rafaseron <rafaelseron3011@gmail.com>
2023-10-29 16:59:26 -0400

    ligacao e log outras view

--- app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
@@ -13,11 +13,14 @@ import kotlin.math.log
 class MainActivity : AppCompatActivity() {
     //inicializamos as variveis e typamos
     lateinit var edtKHWPrice: EditText
+    lateinit var edtKMPercorrido: EditText
     lateinit var btnCalculate: Button
-    lateinit var radioGroup: RadioGroup
-    lateinit var checkBox1: CheckBox
-    lateinit var checkBox2: CheckBox
-    lateinit var switch: Switch
+
+    //Treino prático de outras Views
+    lateinit var rgFood: RadioGroup
+    lateinit var cb_Compromisso1: CheckBox
+    lateinit var cb_Compromisso2: CheckBox
+    lateinit var swLampada: Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,16 +28,38 @@ class MainActivity : AppCompatActivity() {
         //fizemos as variaveis (já typadas) receberem valores (no caso, Views)
         //dessa maneira ligamos a View com o codigo em Kotlin (com as variaveis)
         fun setupViews (){
-        edtKHWPrice = findViewById(R.id.edtKHWPrice)
-        btnCalculate = findViewById(R.id.btnCalculate)
+            edtKHWPrice = findViewById(R.id.edtKHWPrice)
+            btnCalculate = findViewById(R.id.btnCalculate)
+            edtKMPercorrido = findViewById(R.id.edtKMPercorrido)
+
+            //Treino prático das outras Views
+            rgFood = findViewById(R.id.rgFood)
+            cb_Compromisso1 = findViewById(R.id.cb_Compromisso1)
+            cb_Compromisso2 = findViewById(R.id.cb_Compromisso2)
+            swLampada = findViewById(R.id.swLampada)
         }
         setupViews()
 
         //agora vamos começar a usar Listeners para monitorar eventos de clicks
         fun setupListeners(){
-        btnCalculate.setOnClickListener{
-            Log.d("BOTAO CALCULATE","texto capturado -> ${edtKHWPrice.text.toString()}")
-        }
+            btnCalculate.setOnClickListener{
+                Log.d("BOTAO CALCULATE","texto capturado -> ${edtKHWPrice.text.toString()}")
+
+                //Treino prático das outras Views
+                when (rgFood.checkedRadioButtonId){
+                    R.id.rbPizza -> Log.d("RADIOGROUP","Pizza")
+                    R.id.rbBurger -> Log.d("RADIOGROUP","Hamburguer")
+                }
+                if (cb_Compromisso1.isChecked){
+                    Log.d("CHECKBOX1","Compromisso 1")
+            }
+                if (cb_Compromisso2.isChecked){
+                    Log.d("CHECKBOX2","Compromisso 2")
+                }
+                if (swLampada.isChecked){
+                    Log.d("SWITCH","Lampada ligada")
+                }
+
         }
         setupListeners()
     }
