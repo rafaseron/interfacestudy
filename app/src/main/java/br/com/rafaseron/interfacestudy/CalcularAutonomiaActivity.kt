d6a7fa2ccc25889d10eb860431524b6ae5dacb1b
rafaseron <rafaelseron3011@gmail.com>
2023-11-08 00:17:42 -0400

    calculo para segunda tela

--- app/src/main/java/br/com/rafaseron/interfacestudy/CalcularAutonomiaActivity.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/CalcularAutonomiaActivity.kt
@@ -1,13 +1,50 @@
 package br.com.rafaseron.interfacestudy
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class CalcularAutonomiaActivity : AppCompatActivity() {
+    //inicializacao tardia das variaveis
+    lateinit var edtKHWPrice: EditText
+    lateinit var edtKMPercorrido: EditText
+    lateinit var btnCalculate: Button
+    lateinit var txtResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calcular_autonomia)
+
+        //ligacao entre view e variaveis
+        fun setupViews (){
+            edtKHWPrice = findViewById(R.id.edtKHWPrice)
+            btnCalculate = findViewById(R.id.btnCalculate)
+            edtKMPercorrido = findViewById(R.id.edtKMPercorrido)
+            txtResult = findViewById(R.id.txtResult)
+
+        }
+        setupViews()
+
+        //colocar um listener no botao pra fazer o calculo
+        fun setupListeners(){
+            btnCalculate.setOnClickListener{
+
+                val price = edtKHWPrice.text.toString().toFloat()
+                val km = edtKMPercorrido.text.toString().toFloat()
+                val custoPorKM = price/km
+
+                Log.d("BOTAO CALCULATE","texto capturado (Preço) -> ${edtKHWPrice.text.toString()}")
+                Log.d("BOTAO CALCULATE","texto capturado (KM Percorrido)-> ${edtKMPercorrido.text.toString()}")
+                Log.d("BOTAO CALCULATE", "Custo por KM Rodado -> ${custoPorKM.toString()}")
+                txtResult.text = custoPorKM.toString()
+
+            }
+
+        }
+        setupListeners()
     }
 
 }
--- app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
@@ -14,42 +14,16 @@ import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     //inicializamos as variveis e typamos
-    lateinit var edtKHWPrice: EditText
-    lateinit var edtKMPercorrido: EditText
     lateinit var btnCalculate: Button
-    lateinit var txtResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //fizemos as variaveis (já typadas) receberem valores (no caso, Views)
-        //dessa maneira ligamos a View com o codigo em Kotlin (com as variaveis)
-        fun setupViews (){
-            edtKHWPrice = findViewById(R.id.edtKHWPrice)
-            btnCalculate = findViewById(R.id.btnCalculate)
-            edtKMPercorrido = findViewById(R.id.edtKMPercorrido)
-            txtResult = findViewById(R.id.txtResult)
-
-        }
-        setupViews()
-
-        //agora vamos começar a usar Listeners para monitorar eventos de clicks
+        //adicionar listener para abrir a nova activity no botao
         fun setupListeners(){
             btnCalculate.setOnClickListener{
                 startActivity(Intent(this,CalcularAutonomiaActivity::class.java))
-                //toda logica de calculo esta ai em baixo
-                /*
-                val price = edtKHWPrice.text.toString().toFloat()
-                val km = edtKMPercorrido.text.toString().toFloat()
-                val custoPorKM = price/km
-
-                Log.d("BOTAO CALCULATE","texto capturado (Preço) -> ${edtKHWPrice.text.toString()}")
-                Log.d("BOTAO CALCULATE","texto capturado (KM Percorrido)-> ${edtKMPercorrido.text.toString()}")
-                Log.d("BOTAO CALCULATE", "Custo por KM Rodado -> ${custoPorKM.toString()}")
-                txtResult.text = custoPorKM.toString()
-
-                 */
         }
 
 }
