c2f26c373a96fadd3e60112c8c30964103d95406
rafaseron <rafaelseron3011@gmail.com>
2023-11-07 22:45:33 -0400

    add CalcularAutonomia Activity

---
+++ app/src/main/java/br/com/rafaseron/interfacestudy/CalcularAutonomiaActivity.kt
@@ -0,0 +1,13 @@
+package br.com.rafaseron.interfacestudy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalcularAutonomiaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calcular_autonomia)
+    }
+
+}
