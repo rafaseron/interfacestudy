d16de81941e21d2aefad9639b37669cc4db0ab4f
rafaseron <rafaelseron3011@gmail.com>
2023-11-08 00:30:45 -0400

    clear imports on MainActivity

--- app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
@@ -3,14 +3,7 @@ package br.com.rafaseron.interfacestudy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.RadioGroup
-import android.widget.Switch
-import android.widget.TextView
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     //inicializamos as variveis e typamos
