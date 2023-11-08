d3e55f676acc628f4d5432bcd1b1e124a54ee6c6
rafaseron <rafaelseron3011@gmail.com>
2023-11-07 23:17:01 -0400

    clear activity main

--- app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/MainActivity.kt
@@ -18,11 +18,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnCalculate: Button
     lateinit var txtResult: TextView
 
-    //Treino prático de outras Views
-    lateinit var rgFood: RadioGroup
-    lateinit var cb_Compromisso1: CheckBox
-    lateinit var cb_Compromisso2: CheckBox
-    lateinit var swLampada: Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,11 +30,6 @@ class MainActivity : AppCompatActivity() {
             edtKMPercorrido = findViewById(R.id.edtKMPercorrido)
             txtResult = findViewById(R.id.txtResult)
 
-            //Treino prático das outras Views
-            rgFood = findViewById(R.id.rgFood)
-            cb_Compromisso1 = findViewById(R.id.cb_Compromisso1)
-            cb_Compromisso2 = findViewById(R.id.cb_Compromisso2)
-            swLampada = findViewById(R.id.swLampada)
         }
         setupViews()
 
@@ -49,51 +39,13 @@ class MainActivity : AppCompatActivity() {
                 val price = edtKHWPrice.text.toString().toFloat()
                 val km = edtKMPercorrido.text.toString().toFloat()
                 val custoPorKM = price/km
-                //desenvolver ideia abaixo quando for aplicar as correcoes no app
-                //val custoRecarga = custoPorKM*km
+
                 Log.d("BOTAO CALCULATE","texto capturado (Preço) -> ${edtKHWPrice.text.toString()}")
                 Log.d("BOTAO CALCULATE","texto capturado (KM Percorrido)-> ${edtKMPercorrido.text.toString()}")
                 Log.d("BOTAO CALCULATE", "Custo por KM Rodado -> ${custoPorKM.toString()}")
                 txtResult.text = custoPorKM.toString()
-                //desenvolver ideia abaixo quando for aplicar as correcoes no app
-                //Log.d("BOTAO CALCULATE", "Custo total dessa Recarga -> ${custoRecarga.toString()} R$")
         }
-            //Treino prático das outras Views
-            //configurando setOnCheckedChangeListener no RadioGroup
-            rgFood.setOnCheckedChangeListener(){
-                    group, checkedId ->
-                when (checkedId){
-                    R.id.rbPizza -> {Log.d("RADIOGROUP FOOD", "Pizza")}
-                    R.id.rbBurger -> {Log.d("RADIOGROUP FOOD", "Burger")}
-                }
-            }
 
-            //cofigurando setOnCheckedChangeListener no checkbox
-            cb_Compromisso1.setOnCheckedChangeListener(){
-                buttonView, isChecked ->
-                if (isChecked){
-                    Log.d("CHECKBOX1","Compromisso 1")
-                }else{
-                    Log.d("CHECKBOX1", "Compromisso 1 desmarcado")
-                }
-            }
-            cb_Compromisso2.setOnCheckedChangeListener(){
-                    buttonView, isChecked ->
-                if (isChecked){
-                    Log.d("CHECKBOX2","Compromisso 2")
-                }else{
-                    Log.d("CHECKBOX2", "Compromisso 2 desmarcado")
-                }
-            }
-            //configurando setOnCheckedChangeListener no switch
-            swLampada.setOnCheckedChangeListener(){
-                buttonView, isChecked ->
-            if (swLampada.isChecked){
-                Log.d("SWITCH","Lampada ligada")
-            }else{
-                Log.d("SWITCH", "Lampada desligada")
-            }
-    }
 }
         setupListeners()
 }
