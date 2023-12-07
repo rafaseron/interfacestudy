b0596b9a097f03cff8647b2a0183b4cb8e6e9ec5
rafaseron <rafaelseron3011@gmail.com>
2023-12-07 00:49:11 -0400

    aula Adapters

--- app/src/main/java/br/com/rafaseron/interfacestudy/adapter/CarAdapter.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/adapter/CarAdapter.kt
@@ -17,7 +17,9 @@ class CarAdapter (private val carros : Array<String>) : RecyclerView.Adapter<Car
         }
     }
 
-    //controle do ViewHolder
+    //CONTROLE do ViewHolder
+
+    //aqui é onde inflamos um layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder {
         //aqui é criada uma variavel view para inflar o layout
         val view = LayoutInflater.from(parent.context).inflate(R.layout.carro_item, parent, false)
@@ -26,11 +28,14 @@ class CarAdapter (private val carros : Array<String>) : RecyclerView.Adapter<Car
         return CarViewHolder(view)
     }
 
+    //é chamado quando um item precisa ser exibido na tela
     override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
         holder.textView.text = carros[position]
     }
 
+    //numero total de itens do conjunto de dados
     override fun getItemCount(): Int = carros.size
+
 
 }
 
