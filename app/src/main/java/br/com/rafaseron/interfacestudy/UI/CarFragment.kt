c920ea6badfb5a0ceadf929e6f93f9d347de3be3
rafaseron <rafaelseron3011@gmail.com>
2023-12-15 01:15:02 -0400

    Fragments

---
+++ app/src/main/java/br/com/rafaseron/interfacestudy/UI/CarFragment.kt
@@ -0,0 +1,21 @@
+package br.com.rafaseron.interfacestudy.UI
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.rafaseron.interfacestudy.R
+
+class CarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.car_fragment,container,false)
+    }
+
+}
