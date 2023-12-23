6bb969f7007c7248cd9b0b75e0945e96e5dd0114
rafaseron <rafaelseron3011@gmail.com>
2023-12-23 10:46:40 -0300

    alguns erros ainda

--- app/src/main/java/br/com/rafaseron/interfacestudy/UI/CarFragmentModels.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/UI/CarFragmentModels.kt
@@ -19,35 +19,40 @@ class CarFragmentModels : Fragment() {
     lateinit var btnCalculate: Button // <- PASSAR ESSA LOGICA
     lateinit var listaCarros: RecyclerView // <- PASSAR ESSA LOGICA
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         //return super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.car_fragment_models,container,false)
+        return inflater.inflate(R.layout.car_fragment_models, container, false)
 
 
         btnCalculate = requireView().findViewById(R.id.btnCalculate) // <- AQUI
         listaCarros = requireView().findViewById(R.id.rvInformacoesFragment) // <- AQUI
 
-        return view
-
-        fun setupListeners(){   // <- AQUI
-            btnCalculate.setOnClickListener{
-                startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
-            }
-        }
         setupListeners()
-
-        fun setupAdapter(){
-            /* CarAdapter */
-            //val dados = CarFactory().list
-
-            val adaptador = CarAdapter(CarFactory().list)
-
-            listaCarros.adapter = adaptador // <- AQUI
-            listaCarros.layoutManager = LinearLayoutManager(this) //tem como definir o layoutmanager aqui e no .xml também (app:layoutManager="androidx.recyclerview.widget.LinearLayoutManager")
-
-        }
         setupAdapter()
 
+
+        return view
     }
 
+    fun setupListeners(){   // <- AQUI
+        btnCalculate.setOnClickListener{
+            startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
+        }
+    }
+
+
+    fun setupAdapter(){
+        /* CarAdapter */
+        //val dados = CarFactory().list
+
+        val adaptador = CarAdapter(CarFactory().list)
+
+        listaCarros.adapter = adaptador // <- AQUI
+        listaCarros.layoutManager = LinearLayoutManager(this) //tem como definir o layoutmanager aqui e no .xml também (app:layoutManager="androidx.recyclerview.widget.LinearLayoutManager")
+
+    }
 }
