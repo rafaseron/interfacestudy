c63e225cf60a806383d39e9abfdf8880ba8e7f89
rafaseron <rafaelseron3011@gmail.com>
2024-01-26 10:27:35 -0400

    SQL Interface added

---
+++ app/src/main/java/br/com/rafaseron/interfacestudy/data/local/StoredCarsInterface.kt
@@ -0,0 +1,26 @@
+package br.com.rafaseron.interfacestudy.data.local
+
+import android.provider.BaseColumns
+
+object StoredCarsInterface {
+
+    object StoredCarData: BaseColumns{
+        const val TABLE_NAME = "car"
+        const val COLUMN_NAME_PRECO = "preco"
+        const val COLUMN_NAME_BATERIA = "bateria"
+        const val COLUMN_NAME_POTENCIA = "potencia"
+        const val COLUMN_NAME_RECARGA = "recarga"
+        const val COLUMN_NAME_URL_PHOTO = "url_photo"
+    }
+
+    const val SQL_CREATE_STORED_CAR_TABLE = "CREATE_TABLE ${StoredCarData.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+            "${StoredCarData.COLUMN_NAME_PRECO} TEXT," +
+            "${StoredCarData.COLUMN_NAME_BATERIA} TEXT," +
+            "${StoredCarData.COLUMN_NAME_POTENCIA} TEXT," +
+            "${StoredCarData.COLUMN_NAME_RECARGA} TEXT," +
+            "${StoredCarData.COLUMN_NAME_URL_PHOTO} TEXT)"
+
+    const val SQL_DELETE_STORED_CAR_TABLE = "DROP TABLE IF EXISTS ${StoredCarData.TABLE_NAME}"
+
+}
