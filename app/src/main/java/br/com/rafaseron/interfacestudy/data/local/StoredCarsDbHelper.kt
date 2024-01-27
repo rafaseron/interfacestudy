dbec663ea61b4da620af5e2de42bb26aa11f1877
rafaseron <rafaelseron3011@gmail.com>
2024-01-27 17:38:31 -0400

    comentarios referente posicao

--- app/src/main/java/br/com/rafaseron/interfacestudy/data/local/StoredCarsDbHelper.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/data/local/StoredCarsDbHelper.kt
@@ -6,13 +6,13 @@ import android.database.sqlite.SQLiteOpenHelper
 import br.com.rafaseron.interfacestudy.data.local.StoredCarsInterface
 
 class StoredCarsDbHelper (context: Context): SQLiteOpenHelper(context, StoredCarsInterface.DATABASE_NAME, null, StoredCarsInterface.DATABASE_VERSION) {
-    override fun onCreate(db: SQLiteDatabase?) {
+    override fun onCreate(db: SQLiteDatabase?) { //db é o p0
         db?.let{
             it.execSQL(StoredCarsInterface.SQL_CREATE_STORED_CAR_TABLE)
         }
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) { //oldVersion é o p1 e newVersion é o p2
         db?.let{
             it.execSQL(StoredCarsInterface.SQL_DELETE_STORED_CAR_TABLE)
             onCreate(db)
