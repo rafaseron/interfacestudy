8a97b3c061c7913520caa57d50bcfde591bdfc94
rafaseron <rafaelseron3011@gmail.com>
2024-02-01 00:11:37 -0400

    try catch added to insertCar

--- app/src/main/java/br/com/rafaseron/interfacestudy/data/local/StoredCarDatabaseManager.kt
+++ app/src/main/java/br/com/rafaseron/interfacestudy/data/local/StoredCarDatabaseManager.kt
@@ -2,23 +2,31 @@ package br.com.rafaseron.interfacestudy.data.local
 
 import android.content.ContentValues
 import android.content.Context
+import android.util.Log
 import br.com.rafaseron.interfacestudy.domain.Carro
 
 class StoredCarDatabaseManager {
 
     fun insertCar (contexto: Context, carro: Carro){
-        val dbHelper = StoredCarDbHelper(contexto)
-        val db = dbHelper.writableDatabase
-        val values = ContentValues().apply {
-            put(StoredCarInterface.StoredCarData.COLUMN_NAME_PRECO, carro.preco)
-            put(StoredCarInterface.StoredCarData.COLUMN_NAME_BATERIA, carro.bateria)
-            put(StoredCarInterface.StoredCarData.COLUMN_NAME_POTENCIA, carro.potencia)
-            put(StoredCarInterface.StoredCarData.COLUMN_NAME_RECARGA, carro.recarga)
-            put(StoredCarInterface.StoredCarData.COLUMN_NAME_URL_PHOTO, carro.urlPhoto)
-        }
-        // val newRegister = db.insert(StoredCarsInterface.StoredCarData.TABLE_NAME, null, values)
-        db.insert(StoredCarInterface.StoredCarData.TABLE_NAME, null, values)
-    }
 
+        try {
+            val dbHelper = StoredCarDbHelper(contexto)
+            val db = dbHelper.writableDatabase
+            val values = ContentValues().apply {
+                put(StoredCarInterface.StoredCarData.COLUMN_NAME_PRECO, carro.preco)
+                put(StoredCarInterface.StoredCarData.COLUMN_NAME_BATERIA, carro.bateria)
+                put(StoredCarInterface.StoredCarData.COLUMN_NAME_POTENCIA, carro.potencia)
+                put(StoredCarInterface.StoredCarData.COLUMN_NAME_RECARGA, carro.recarga)
+                put(StoredCarInterface.StoredCarData.COLUMN_NAME_URL_PHOTO, carro.urlPhoto)
+            }
+            db.insert(StoredCarInterface.StoredCarData.TABLE_NAME, null, values)
+
+        }catch (excecao: Exception){
+            excecao.message?.let {
+                Log.e("DB Insert Error", it)
+            }
+        }
+
+    }
 
 }
